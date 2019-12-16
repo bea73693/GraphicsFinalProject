7c39cd9bd86ba22d27dbe66bb7c2e334baf7314b
bea73693 <56211155+bea73693@users.noreply.github.com>
2019-12-16 13:54:31 -0600

    Update 1.0

--- vshader.glsl
+++ vshader.glsl
@@ -2,19 +2,31 @@
 precision mediump float;
 uniform mat4 mv;
 uniform mat4 proj;
+uniform sampler2D textureSampler;
+uniform int squaresActive;
+
 
 in vec4 vPosition;
+in vec4 vSquarePosition;
+in vec2 texCoord;
 in vec4 vNormal;
 in vec4 vColor;
+in vec4 vSquareColor;
 out vec4 color;
 
 void main(){
-    gl_Position = proj*mv*vPosition;
     gl_PointSize = 1.0;
-    //as a last ditch debugging strategy we can treat any vector as a color vector.
-    //red is our x coordinate, green is our y coordinate, blue is our z coordinate
-    color = vColor; //Normal vector has w coordinate of 0
-    color.a = 1.0;
-    //color = vPosition ;
+
+    //squares
+    if(squaresActive == 1){
+        gl_Position = proj*mv*vSquarePosition;
+        color = vSquareColor;
+        color.a = texture(textureSampler, texCoord).a;
+    }
+    else{
+        gl_Position = proj*mv*vPosition;
+        color = vColor;
+        color.a = 1.0;
+    }
 
 }
